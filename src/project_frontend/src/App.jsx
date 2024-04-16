710711263c0a738e9486c9d233460b6e9f78572e
Swa-ne <spaulbautista@gmail.com>
2024-04-16 20:36:08 +0800

    craeted Home page

--- src/project_frontend/src/App.jsx
+++ src/project_frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import CreateCampaign from './pages/CreateCampaign';
 import Navbar from './components/Navbar';
 import Sidebar from './components/Sidebar';
+import Home from './pages/home';
 
 function App() {
   return (
@@ -14,8 +15,8 @@ function App() {
         <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
           <Navbar />
           <Routes>
-            {/* <Route path="/" element={<Home />} />
-            <Route path="/profile" element={<Profile />} /> */}
+            <Route path="/" element={<Home />} />
+            {/* <Route path="/profile" element={<Profile />} /> */}
             <Route path="/create-campaign" element={<CreateCampaign />} />
             {/* <Route path="/campaign-details/:id" element={<CampaignDetails />} /> */}
           </Routes>
---
+++ src/project_frontend/src/context/index.tsx
@@ -0,0 +1,104 @@
+import React, { useContext, createContext, ReactNode } from 'react';
+
+// import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+// import { ethers } from 'ethers';
+// import { EditionMetadataWithOwnerOutputSchema } from '@thirdweb-dev/sdk';
+
+const StateContext = createContext<any>(null);
+interface StateContextProviderProps {
+  children: ReactNode;
+}
+export const StateContextProvider = ({ children }: StateContextProviderProps) => {
+  // const { contract } = useContract('0xf59A1f8251864e1c5a6bD64020e3569be27e6AA9');
+  // const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+  const contract = () => {};//useAddress();
+  const address = () => {};//useAddress();
+  const connect = () => {};//useMetamask();
+
+  const publishCampaign = async (form: any) => {
+    try {
+      // const data = await createCampaign({
+			// 	args: [
+			// 		address, // owner
+			// 		form.title, // title
+			// 		form.description, // description
+			// 		form.target,
+			// 		new Date(form.deadline).getTime(), // deadline,
+			// 		form.image,
+			// 	],
+			// });
+
+      // console.log("contract call success", data)
+    } catch (error) {
+      console.log("contract call failure", error)
+    }
+  }
+
+  const getCampaigns = async () => {
+    // const campaigns = await contract.call('getCampaigns');
+
+    // const parsedCampaings = campaigns.map((campaign: any, i: any) => ({
+    //   owner: campaign.owner,
+    //   title: campaign.title,
+    //   description: campaign.description,
+    //   target: ethers.utils.formatEther(campaign.target.toString()),
+    //   deadline: campaign.deadline.toNumber(),
+    //   amountCollected: ethers.utils.formatEther(campaign.amountCollected.toString()),
+    //   image: campaign.image,
+    //   pId: i
+    // }));
+
+    // return parsedCampaings;
+  }
+
+  const getUserCampaigns = async () => {
+    // const allCampaigns = await getCampaigns();
+
+    // const filteredCampaigns = allCampaigns.filter((campaign: any) => campaign.owner === address);
+
+    // return filteredCampaigns;
+  }
+
+  const donate = async (pId: any, amount: any) => {
+    // const data = await contract.call('donateToCampaign', [pId], { value: ethers.utils.parseEther(amount)});
+
+    // return data;
+  }
+
+  const getDonations = async (pId: any) => {
+    // const donations = await contract.call('getDonators', [pId]);
+    // const numberOfDonations = donations[0].length;
+
+    // const parsedDonations = [];
+
+    // for(let i = 0; i < numberOfDonations; i++) {
+    //   parsedDonations.push({
+    //     donator: donations[0][i],
+    //     donation: ethers.utils.formatEther(donations[1][i].toString())
+    //   })
+    // }
+
+    // return parsedDonations;
+  }
+
+
+  return (
+    <StateContext.Provider
+      value={{ 
+        address,
+        contract,
+        connect,
+        createCampaign: publishCampaign,
+        getCampaigns,
+        getUserCampaigns,
+        donate,
+        getDonations
+      }}
+    >
+      {children}
+    </StateContext.Provider>
+  )
+}
+
+export const useStateContext = () => useContext(StateContext);
