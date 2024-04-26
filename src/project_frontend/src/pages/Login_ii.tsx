f8a20303d506aca9cfcadd19fb793801aa126f37
jairasolis <jairasolis030@gmail.com>
2024-04-26 17:47:51 +0800

    internet identity integration

--- src/project_frontend/src/pages/Login_ii.tsx
+++ src/project_frontend/src/pages/Login_ii.tsx
@@ -1,81 +1,80 @@
-// import React, { useState } from "react";
-// import {
-//   project_backend,
-//   createActor,
-// } from "../../../declarations/project_backend";
-// import { AuthClient } from "@dfinity/auth-client";
-// import { HttpAgent } from "@dfinity/agent";
+import React, { useState } from "react";
+import {
+  project_backend,
+  createActor,
+} from "../../../declarations/project_backend";
+import { AuthClient } from "@dfinity/auth-client";
+import { HttpAgent } from "@dfinity/agent";
 
-// const Login_ii: React.FC = () => {
-//   const [actor, setActor] = useState(project_backend);
-//   console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
+const Login_ii: React.FC = () => {
+  const [actor, setActor] = useState(project_backend);
+  console.log(process.env.CANISTER_ID_INTERNET_IDENTITY);
 
-//   const handleWhoAmIClick = async (
-//     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-//   ) => {
-//     e.preventDefault();
-//     const whoAmIButton = e.currentTarget;
-//     whoAmIButton.setAttribute("disabled", "true");
-//     try {
-//       const principal = await actor.getMe();
-//       document.getElementById("principal")!.innerText = principal.toString();
-//     } catch (error) {
-//       console.error("Error retrieving principal:", error);
-//     } finally {
-//       whoAmIButton.removeAttribute("disabled");
-//     }
-//   };
+  const handleWhoAmIClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    const whoAmIButton = e.currentTarget;
+    whoAmIButton.setAttribute("disabled", "true");
+    try {
+      const principal = await actor.getMe();
+      document.getElementById("principal")!.innerText = principal.toString();
+    } catch (error) {
+      console.error("Error retrieving principal:", error);
+    } finally {
+      whoAmIButton.removeAttribute("disabled");
+    }
+  };
 
-//   const handleLoginClick = async (
-//     e: React.MouseEvent<HTMLButtonElement, MouseEvent>
-//   ) => {
-//     e.preventDefault();
-//     try {
-//       let authClient = await AuthClient.create();
-//       await new Promise<void>((resolve) => {
-//         authClient.login({
-//           identityProvider:
-//             process.env.DFX_NETWORK === "ic"
-//               ? "https://identity.ic0.app"
-//               : //: "http://rdmx6-jaaaa-aaaaa-aaadq-cai.localhost:4943",
-//                 `http://localhost:3000/?canisterId=bd3sg-teaaa-aaaaa-qaaba-cai`, // canister id of our internet identity service [must replace this pa, idk where to find the url]
-//           onSuccess: resolve,
-//         });
-//       });
-//       const identity = authClient.getIdentity();
-//       console.log(identity);
-//       const agent = new HttpAgent({ identity });
-//       const newActor = createActor(
-//         process.env.CANISTER_ID_II_INTEGRATION_BACKEND,
-//         {
-//           agent,
-//         }
-//       );
-//       setActor(newActor);
-//     } catch (error) {
-//       console.error("Error logging in:", error);
-//     }
-//   };
+  const handleLoginClick = async (
+    e: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    e.preventDefault();
+    try {
+      let authClient = await AuthClient.create();
+      await new Promise<void>((resolve) => {
+        authClient.login({
+          identityProvider:
+            process.env.DFX_NETWORK === "ic"
+              ? "https://identity.ic0.app"
+              : "http://cuj6u-c4aaa-aaaaa-qaajq-cai.localhost:4943",
+          onSuccess: resolve,
+        });
+      });
+      const identity = authClient.getIdentity();
+      console.log(identity);
+    //   const agent = new HttpAgent({ identity });
+    //   const newActor = createActor(
+    //     process.env.CANISTER_ID_II_INTEGRATION_BACKEND,
+    //     {
+    //       agent,
+    //     }
+    //   );
+    //   setActor(newActor);
+    } catch (error) {
+      console.error("Error logging in:", error);
+    }
+  };
 
-//   return (
-//     <main>
-//       <img src="logo2.svg" alt="DFINITY logo" />
-//       <br />
-//       <br />
-//       <form>
-//         <button id="login" onClick={handleLoginClick}>
-//           Login!
-//         </button>
-//       </form>
-//       <br />
-//       <form>
-//         <button id="whoAmI" onClick={handleWhoAmIClick}>
-//           Who Am I
-//         </button>
-//       </form>
-//       <section id="principal"></section>
-//     </main>
-//   );
-// };
+  return (
+    <main>
+      <img src="logo2.svg" alt="DFINITY logo" />
+      <br />
+      <br />
+      <form>
+        <button id="login" onClick={handleLoginClick}>
+          Login!
+        </button>
+      </form>
+      <br />
+      <form>
+        <button id="whoAmI" onClick={handleWhoAmIClick}>
+          Who Am I
+        </button>
+      </form>
+      <section id="principal"></section>
+    </main>
+  );
+};
 
-// export default Login_ii;
+export default Login_ii;
