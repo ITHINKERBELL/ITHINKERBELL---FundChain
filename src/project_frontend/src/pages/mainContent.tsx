010b94169314a272bd0ed49877d4a2405c0c396b
Swa-ne <spaulbautista@gmail.com>
2024-05-04 17:35:54 +0800

    Connect the frontend wallet connection to the backend.

--- src/project_frontend/src/pages/mainContent.tsx
+++ src/project_frontend/src/pages/mainContent.tsx
@@ -1,19 +1,39 @@
+import { useEffect, useState } from "react";
 import { useAccount } from "wagmi";
 import UserPage from "../layout/User";
 import DonorPage from "../layout/Donor";
 import DefaultPage from "../layout/Default";
 import Register from "./Register";
 
+import { project_backend } from "../../../declarations/project_backend";
 function MainContent() {
 
   const { address, isConnected } = useAccount()
+  let [usertype, setUsertype] = useState<string | null>(null);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        setUsertype(await project_backend.getUserTypeByWalletAddress(`${address}`)
+          .then((res) => {
+            return res
+          })
+          .catch((error) => {
+            return null
+          }));
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
 
-  let usertype = "user"
-
+    // Call the async function immediately
+    fetchData();
+  }, [])
+  // TODO: Create a loading page before uncommenting this 
+  // if (isConnected && usertype === null) return <><Loading /></>
   if (isConnected && usertype === "user") return <><UserPage /></>
-  if (isConnected && usertype === "donor") return <><DonorPage /></>
-  if (isConnected && usertype === null) return <><Register /></>
-  return <><DefaultPage /></>
+  else if (isConnected && usertype === "donor") return <><DonorPage /></>
+  else if (isConnected && usertype === "User not found") return <><Register /></>
+  else return <><DefaultPage /></>
 }
 
 export default MainContent;
