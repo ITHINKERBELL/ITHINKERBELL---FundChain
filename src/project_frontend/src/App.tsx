b397b619adf9f98920da3abdc0eb220d19290fe9
Swa-ne <spaulbautista@gmail.com>
2024-05-04 16:19:21 +0800

    Remove old authentication on App.tsx

--- src/project_frontend/src/App.tsx
+++ src/project_frontend/src/App.tsx
@@ -1,22 +1,10 @@
-import React, { useEffect, useState } from "react";
 import MainContent from "./pages/mainContent";
-import { authenticateToken } from "./services/authentication";
 import { ActorProvider } from "./context/ActorContext";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-
-  // useEffect(() => {
-  //   const fetchData = async () => {
-  //     const a = await authenticateToken();
-  //     setIsLoggedIn(a);
-  //   };
-  //   fetchData();
-  // }, []);
-
   return (
     <ActorProvider>
-      {isLoggedIn ? <MainContent /> : <MainContent />}
+      <MainContent />
     </ActorProvider>
   );
 }
