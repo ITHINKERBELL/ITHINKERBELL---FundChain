155abf4a31c3d315a4128a12091fcfe1103ef76e
Swa-ne <spaulbautista@gmail.com>
2024-05-04 17:39:41 +0800

    Users will now need to register their details when it's their first time connecting their wallet.

--- src/project_frontend/src/pages/Register.tsx
+++ src/project_frontend/src/pages/Register.tsx
@@ -1,43 +1,24 @@
 import React, { useEffect, useState } from 'react';
-import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 import {
   project_backend,
-  createActor,
 } from "../../../declarations/project_backend";
-import { useActor } from "../context/ActorContext";
-import { Principal } from '@dfinity/principal';
+import { useAccount } from 'wagmi';
 
 
-// TODO: connect the form to userRegistration function in our backend
 
 const Register: React.FC = () => {
-  // const [actor, setActor] = useState(project_backend);
-  const { actor } = useActor();
-  const navigate = useNavigate();
-  const [res, setRes] = useState<any>(null);
-  const [principal, setPrincipal] = useState<any>(null);
+  const [err, setErr] = useState<string | null>(null);
+  const { address } = useAccount()
   const [formData, setFormData] = useState({
+    wallet: address,
     email: '',
     username: '',
     userType: '',
-    wallet: '',
     firstName: '',
-    middleName: '',
     lastName: '',
+    middleName: '',
     birthday: '',
   });
-
-  useEffect(() => {
-    console.log(actor);
-    getMe();
-  }, []);
-
-  const getMe = async () => {
-    const principal_id = await actor.getMe();
-    setPrincipal(principal_id);
-    console.log(principal_id.toString());
-  };
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -49,56 +30,24 @@ const Register: React.FC = () => {
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
-    const form = e.currentTarget;
-
-    const emailInput = form.elements.namedItem(
-      "email"
-    ) as HTMLInputElement;
-    const usernameInput = form.elements.namedItem(
-      "username"
-    ) as HTMLInputElement;
-    const userTypeInput = form.elements.namedItem(
-      "userType"
-    ) as HTMLInputElement;
-    const walletInput = form.elements.namedItem(
-      "wallet") as HTMLInputElement;
-    const firstNameInput = form.elements.namedItem(
-      "firstName"
-    ) as HTMLInputElement;
-    const middleNameInput = form.elements.namedItem(
-      "middleName") as HTMLInputElement;
-    const lastNameInput = form.elements.namedItem(
-      "lastName"
-    ) as HTMLInputElement;
-    const birthdayInput = form.elements.namedItem(
-      "birthday") as HTMLInputElement;
-
-    const emailValue = emailInput.value;
-    const usernameValue = usernameInput.value;
-    const userTypeValue = userTypeInput.value;
-    const walletValue = walletInput.value;
-    const firstNameValue = firstNameInput.value;
-    const middleNameValue = middleNameInput.value;
-    const lastNameValue = lastNameInput.value;
-    const birthdayValue = birthdayInput.value;
-
     project_backend
       .userRegistration(
-        principal,
-        emailValue,
-        usernameValue,
-        userTypeValue,
-        walletValue,
-        firstNameValue,
-        middleNameValue,
-        lastNameValue,
-        birthdayValue
+        `${formData.wallet}`,
+        `${formData.email}`,
+        `${formData.username}`,
+        `${formData.userType}`,
+        `${formData.firstName}`,
+        `${formData.lastName}`,
+        `${formData.middleName}`,
+        `${formData.birthday}`,
       )
       .then((res) => {
         console.log(res);
-        console.log("Registered successfully! Please login.")
-        setRes(res);
-        navigate("/auth");
+        if (res === "success") {
+          document.location.reload()
+        } else {
+          setErr(res)
+        }
       })
       .catch((error) => {
         console.error(error);
@@ -110,6 +59,7 @@ const Register: React.FC = () => {
     <div className="max-w-md mx-auto mt-10 p-6 bg-white rounded-md shadow-md">
       <h2 className="text-2xl font-semibold mb-4">Register</h2>
       <form onSubmit={handleSubmit}>
+        {err && <label className='text-red-800'>{err}</label>}
         <div className="mb-4">
           <label htmlFor="email" className="block text-gray-700 font-semibold mb-2">
             Email
@@ -139,6 +89,7 @@ const Register: React.FC = () => {
           />
         </div>
         <div className="mb-4">
+          {/* TODO: change to radio button donor and organizer (?) as choices */}
           <label htmlFor="userType" className="block text-gray-700 font-semibold mb-2">
             User Type
           </label>
@@ -147,24 +98,6 @@ const Register: React.FC = () => {
             id="userType"
             name="userType"
             value={formData.userType}
-            onChange={handleChange}
-            className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-            required
-          />
-        </div>
-        <div className="mb-4">
-          <label htmlFor="wallet" className="block text-gray-700 font-semibold mb-2">
-            Wallet
-          </label>
-          <Link to="https://nns.ic0.app/" target="_blank" rel="noopener noreferrer">
-            <div className='bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 focus:outline-none focus:bg-blue-600 mb-2'> Get wallet address </div>
-          </Link>
-
-          <input
-            type="text"
-            id="wallet"
-            name="wallet"
-            value={formData.wallet}
             onChange={handleChange}
             className="w-full px-4 py-2 border rounded-md focus:outline-none focus:border-blue-500"
             required
