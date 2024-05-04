069443438c2554b2efa0bb7e73e04544606a7274
Swa-ne <spaulbautista@gmail.com>
2024-05-04 14:44:32 +0800

    Remove unutilized functions from index.ts

--- src/project_backend/src/index.ts
+++ src/project_backend/src/index.ts
@@ -7,9 +7,9 @@ import { encrypt, validate } from './util/authentication';
 
 // Define the User record type
 const User = Record({
-    id: Principal,
     email: text,
     username: text,
+    wallet: Principal,
     password: text,
     userType: text,
     name: Record({
@@ -22,25 +22,7 @@ const User = Record({
 })
 
 export type User = typeof User.tsType;
-export let users = StableBTreeMap<Principal, User>(0)
-
-const User_II = Record({
-    id: Principal,
-    email: text,
-    username: text,
-    userType: text,
-    wallet: text,
-    name: Record({
-        firstName: text,
-        lastName: text,
-        middleName: text,
-        birthday: text
-    }),
-    latestLoginDate: text
-})
-
-export type User_II = typeof User_II.tsType;
-export let users_II = StableBTreeMap<Principal, User_II>(4)
+export let users = StableBTreeMap<Principal, User>(4)
 
 const Campaigns = Record({
     campaignId: text,
@@ -67,17 +49,34 @@ export default Canister({
     greet: query([text], text, (name) => {
         return `Hello, ${name}!`;
     }),
-    userRegistration: update([text, text, text, text, text, text, text, text], text, async (email, username, password, userType, firstName, lastName, middleName, birthday) => {
+    userLogin: update([Principal], text, async (principal) => {
+        try {
+            if (!users.containsKey(principal)) {
+                {
+                    return `User doesn't exist.`;
+                }
+            }
+
+            const user = users.get(principal);
+            return JSON.stringify({ message: 'success', user: user });
+        } catch (error) {
+            {
+                return `InternalError: ${{ error }}`;
+            }
+        }
+    }),
+    userRegistration: update([Principal, text, text, text, text, text, text, text, text], text, async (wallet, email, username, password, userType, firstName, lastName, middleName, birthday) => {
         // Checks whether the user's entered username, email, and password are valid and available
         const checkerForInput = await checkEveryInputForSignup(username, email, password, userType, birthday);
         if (checkerForInput.message === "success") {
-            const id = generateId();
+            // get current user wallet
+            // const wallet = ic.caller();
             // Bcrypt is used to encrypt the entered password
-            const newUser: User = {
-                id,
+            const regUser: User = {
+                wallet,
                 email,
                 username,
-                password: hash(password, `${id}`),
+                password: hash(password, `${wallet}`),
                 userType,
                 name: {
                     firstName,
@@ -87,7 +86,7 @@ export default Canister({
                 },
                 latestLoginDate: JSON.stringify(new Date())
             };
-            users.insert(newUser.id, newUser)
+            users.insert(regUser.wallet, regUser)
         }
         return checkerForInput.message
     }),
@@ -110,25 +109,6 @@ export default Canister({
             }
             return JSON.stringify(foundUser);
         }
-    }),
-    userLogin: update([text, text], text, async (email, password) => {
-        if (!checkEmailValidity(email)) {
-            return 'Invalid email address.'
-        }
-        let allUsers = users.values();
-        for (let user of allUsers) {
-            {
-                if (user.email.toLowerCase() === email.toLowerCase()) {
-                    {
-                        if (compareHash(password, user.password, `${user.id}`)) {
-                            return `{ "message": "Success", "token": "${await encrypt(user)}", "userType": "${user.userType}", "username": "${user.username}", "email": "${user.email}", "id": "${user.id}", "userFullName": "${user.name.firstName} ${user.name.middleName} ${user.name.lastName}" }`;
-                        }
-                        return `Incorrect email or password.`
-                    }
-                }
-            }
-        }
-        return 'Incorrect email or password.'
     }),
     validateToken: query([text, text], text, async (token: string, email: string) => {
         try {
@@ -171,18 +151,15 @@ export default Canister({
 
         return newCampaign;
     }),
-
     // debugging 
     getCampaignsLength: update([], text, () => {
         const numCampaigns = campaigns.len();
 
         return `${numCampaigns} number of campaigns`;
     }),
-
     getAllCampaigns: query([], Vec(Campaigns), () => {
         return campaigns.values();
     }),
-
     getCampaignById: query([CampaignId], Opt(Campaigns), (_campaignId: CampaignId) => {
         return campaigns.get(_campaignId);
     }),
@@ -190,90 +167,38 @@ export default Canister({
     // getCampaignByTitle: query([CampaignTitle], Opt(Campaigns), (_campaignTitle: CampaignTitle) => {
     //     return campaigns.get(_campaignTitle);
     // }),
-
     getMe: query([], Principal, () => {
-        let currentPrincipal = ic.caller();
-   
-            // If user does not exist, return error.
-            if (!users_II.containsKey(currentPrincipal)) {
-                return currentPrincipal;
-            }
-   
-            // Return the current user.
-            const user = users_II.get(currentPrincipal);
-            // return JSON.stringify(user); // Assuming you want to return the user as a JSON string
-            return currentPrincipal;
-
-    }),
-
-    userChecker: query([Principal], text, async (principal) => {
         let currentPrincipal = ic.caller();
 
         // If user does not exist, return error.
-        if (!users_II.containsKey(principal)) {
+        if (!users.containsKey(currentPrincipal)) {
+            return currentPrincipal;
+        }
+
+        // Return the current user.
+        const user = users.get(currentPrincipal);
+        // return JSON.stringify(user); // Assuming you want to return the user as a JSON string
+        return currentPrincipal;
+
+    }),
+    userChecker: query([Principal], text, async (principal) => {
+        // If user does not exist, return error.
+        if (!users.containsKey(principal)) {
             return "unregistered";
-        }else if(users_II.containsKey(principal)){
+        } else if (users.containsKey(principal)) {
             const principall = principal.toString();
             return `registered: ${principall}`
         }
 
         // Return the current user.
-        const user = users_II.get(principal);
+        const user = users.get(principal);
         const userr = user.toString();
         // return JSON.stringify(user); // Assuming you want to return the user as a JSON string
         return userr;
 
     }),
-
     userDetails: query([Principal], text, async (principal: Principal) => {
-        const user = users_II.get(principal);
-        return JSON.stringify({ message: 'success', user: user }); 
-    }),
-
-    getAllUsers_II: query([], Vec(User_II), () => {
-        return users_II.values()
-    }),
-
-    user_II_Registration: update([Principal, text, text, text, text, text, text, text, text], User_II, async (id, email, username, userType, wallet, firstName, lastName, middleName, birthday) => {
-
-        const newUser_II: User_II = {
-            id,
-            email,
-            username,
-            userType,
-            wallet,
-            name: {
-                firstName,
-                lastName,
-                middleName,
-                birthday
-            },
-            latestLoginDate: JSON.stringify(new Date())
-        };
-        users_II.insert(newUser_II.id, newUser_II)
-
-        return newUser_II;
-    }),
-
-    user_II_Login: update([Principal], text, async (principal) => {{
-        try {{
-            if (!users_II.containsKey(principal)) {{
-                return `User doesn't exist.`;
-            }}
-    
-            const user = users_II.get(principal);
-            return JSON.stringify({ message: 'success', user: user }); 
-        }} catch (error) {{
-            return `InternalError: ${{error}}`;
-        }}
-    }}),
-
+        const user = users.get(principal);
+        return JSON.stringify({ message: 'success', user: user });
+    })
 })
-
-function generateId(): Principal {
-    const randomBytes = new Array(29)
-        .fill(0)
-        .map((_) => Math.floor(Math.random() * 256));
-
-    return Principal.fromUint8Array(Uint8Array.from(randomBytes));
-}
