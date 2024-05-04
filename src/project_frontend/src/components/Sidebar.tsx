a59d831c39d953e0a93c052f6b92a6172a845924
Monica Villanueva <127196097+vllnvmonik@users.noreply.github.com>
2024-05-04 12:09:11 +0800

    revised sidebar

--- src/project_frontend/src/components/Sidebar.tsx
+++ src/project_frontend/src/components/Sidebar.tsx
@@ -1,68 +1,87 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-  faHouse,
+  faHouseChimney,
   faScroll,
-  faUser,
+  faCircleUser,
   faRightFromBracket,
   faSquarePlus,
 } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import { logout } from "../services/entry";
 import { useActor } from "../context/ActorContext";
+import logo from "../../public/logo2.svg";
 
 const Sidebar: React.FC = () => {
 
   const { resetActor } = useActor();
   const navigate = useNavigate();
+  // const handleLogoutClick = () => {
+  //   navigate("/auth");
+  //   // resetActor();
+  // };
 
-  const handleLogoutClick = () => {
-    navigate("/auth");
-    // resetActor();
+  const [activeLink, setActiveLink] = useState<string | null>(null);
+
+  const handleMenuItemClick = (path: string) => {
+    setActiveLink(path);
+    if (path === "/auth") {
+      logout();
+      navigate(path);
+    }
   };
 
   return (
-    <aside className="h-screen w-16 flex flex-col space-y-10 items-center justify-center absolute top-0 left-0 bg-gradient-to-br from-blue-500 to-purple-600 text-white rounded-tr-xl rounded-br-xl">
-      
-      {/*home*/}
+    <aside className="w-50 h-screen flex flex-col justify-start fixed top-0 left-0 bg-[#FFFFFF] text-white">
+      <div className="flex items-center justify-center h-14 w-14 rounded-full mb-10 bg-[#E4E4E4] m-6">
+        <img src={logo} alt="logo" />
+      </div>
+
       <Link to="/">
-        <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:ease-linear focus:bg-white">
-          <FontAwesomeIcon icon={faHouse} />
+        <div
+          className={`flex items-center justify-start p-3 mr-2 h-10 rounded-tr-lg rounded-br-lg cursor-pointer text-[#2D2D2D] hover:bg-[#2D2D2D] hover:text-white hover:duration-300 hover:ease-linear focus:bg-[#2D2D2D] mb-2 ${
+            activeLink === "/" ? "bg-[#2D2D2D] text-white" : ""
+          }`}
+          onClick={() => handleMenuItemClick("/")}
+        >
+          <FontAwesomeIcon className="ml-2 text-sm" icon={faHouseChimney} />
+          <p className="text-xs ml-4 p-2">Campaigns</p>
         </div>
       </Link>
 
-      {/*add campaigns*/}
       <Link to="/create-campaign">
-          <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:ease-linear focus:bg-white">
-            <FontAwesomeIcon icon={faSquarePlus} />
-          </div>
-      </Link>
-
-      {/*campaigns*/}
-      <Link to="/">
-        <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:ease-linear focus:bg-white">
-          <FontAwesomeIcon icon={faScroll} />
+        <div
+          className={`flex items-center justify-start p-3 mr-2 h-10 rounded-tr-lg rounded-br-lg cursor-pointer text-[#2D2D2D] hover:bg-[#2D2D2D] hover:text-white hover:duration-300 hover:ease-linear focus:bg-[#2D2D2D] mb-2 ${
+            activeLink === "/create-campaign" ? "bg-[#2D2D2D] text-white" : ""
+          }`}
+          onClick={() => handleMenuItemClick("/create-campaign")}
+        >
+          <FontAwesomeIcon className="ml-2 text-sm" icon={faSquarePlus} />
+          <p className="text-xs ml-4 p-2">Create Campaigns</p>
         </div>
       </Link>
 
       <Link to="/profile">
-      {/*user*/}
-        <div className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:ease-linear focus:bg-white">
-          <FontAwesomeIcon icon={faUser} />
+        <div
+          className={`flex items-center justify-start p-3 mr-2 h-10 rounded-tr-lg rounded-br-lg cursor-pointer text-[#2D2D2D] hover:bg-[#2D2D2D] hover:text-white hover:duration-300 hover:ease-linear focus:bg-[#2D2D2D] mb-2 ${
+            activeLink === "/profile" ? "bg-[#2D2D2D] text-white" : ""
+          }`}
+          onClick={() => handleMenuItemClick("/profile")}
+        >
+          <FontAwesomeIcon className="ml-2 text-sm" icon={faCircleUser} />
+          <p className="text-xs ml-4 p-2">Profile</p>
         </div>
       </Link>
 
-      {/*logout*/}
       <div
-        onClick={() => {
-          logout();
-          handleLogoutClick();
-          // window.location.reload();
-        }}
-        className="h-10 w-10 flex items-center justify-center rounded-lg cursor-pointer hover:bg-white hover:text-gray-800 hover:duration-300 hover:ease-linear focus:bg-white"
+        className={`mt-auto flex items-center justify-start p-3 mr-2 h-10 rounded-tr-lg rounded-br-lg cursor-pointer text-[#2D2D2D] hover:bg-[#2D2D2D] hover:text-white hover:duration-300 hover:ease-linear focus:bg-[#2D2D2D] ${
+          activeLink === "/auth" ? "bg-[#2D2D2D] text-white" : ""
+        }`}
+        onClick={() => handleMenuItemClick("/auth")}
       >
-        <FontAwesomeIcon icon={faRightFromBracket} />
+        <FontAwesomeIcon className="ml-2 text-sm" icon={faRightFromBracket} />
+        <p className="text-xs ml-4 p-2">Logout</p>
       </div>
     </aside>
   );
--- src/project_frontend/src/pages/mainContent.tsx
+++ src/project_frontend/src/pages/mainContent.tsx
@@ -12,20 +12,22 @@ function MainContent() {
 
   return (
     <div className="relative bg-gray-100 min-h-screen flex flex-row">
-      <div className="sm:flex hidden mr-10 fixed">
+      <div className="hidden sm:flex mr-10">
         <Sidebar />
       </div>
 
-      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pr-5">
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pl-56">
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/create-campaign" element={<CreateCampaign />} />
-          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/register" element={<Register_ii />} />
-        </Routes>
+        <div className="pl-5">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/create-campaign" element={<CreateCampaign />} />
+            <Route path="/campaign-details/:id" element={<CampaignDetails />} />
+            <Route path="/auth" element={<Auth />} />
+            <Route path="/register" element={<Register_ii />} />
+          </Routes>
+        </div>
       </div>
     </div>
   );
