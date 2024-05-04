a395dbce50afabb3f01efac815a967c47e8cec1c
jairasolis <jairasolis030@gmail.com>
2024-05-04 19:27:35 +0800

    fix user page

--- src/project_frontend/src/layout/User.tsx
+++ src/project_frontend/src/layout/User.tsx
@@ -11,9 +11,9 @@ function UserPage() {
     <div className="relative bg-gray-100 min-h-screen flex flex-row">
       <div className="hidden sm:flex mr-10">
         <Sidebar />
-        <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pl-40">
-          <div className="pl-5">
-          </div>
+      </div>
+      <div className="flex-1 max-sm:w-full max-w-[1280px] mx-auto sm:pl-40">
+        <div className="pl-5">
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/profile" element={<Profile />} />
